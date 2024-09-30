5e16d6d48fdbb27b830b89b6489ac7e1c6474390
kulagin <alex_kulagin@samgk.ru>
2024-09-30 17:21:19 +0400

    добавил реализацию

--- ru-text-spam-detector-ml.net/SpamRecognition.cs
+++ ru-text-spam-detector-ml.net/SpamRecognition.cs
@@ -1,10 +1,19 @@
-﻿namespace ru_text_spam_detector_ml.net;
+﻿using Ru_text_spam_detector_ml_net;
+
+namespace ru_text_spam_detector_ml.net;
 
 public class SpamRecognition
 {
     public float Recognize(string? message)
     {
-        return 0;
+        //Load sample data
+        var sampleData = new RuSpamMl.ModelInput
+        {
+            Message = message ?? string.Empty
+        };
+        
+        //Load model and predict output
+        return RuSpamMl.Predict(sampleData).Score.Last();
     }
 }
 
